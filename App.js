ed18d4eb7812bebe4ed6db44251ebc8d6e24ed27
Abdulelah-Abacar <abdulelahabacar@gmail.com>
2024-01-15 21:34:09 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from "expo-font";
+
+// Screens
+import Home from "./screens/Home";
+import Doctors from "./screens/Doctors";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import Profile from "./screens/Profile";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [loaded] = useFonts({
+    NotoFont: require('./assets/fonts/NotoKufiArabic-Regular.ttf'),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#007bff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            textAlign: "right",
+            fontFamily: "NotoFont"
+          },
+        }}
+      >
+        <Stack.Screen 
+          name="Home"
+          options={{headerShown: false}}
+          component={Home}
+        />
+        <Stack.Screen 
+          name="SignIn"
+          options={{
+            title: "تسجيل الدخول"
+          }}
+          component={SignIn}
+        />
+        <Stack.Screen 
+          name="SignUp"
+          options={{
+            title: "حساب جديد"
+          }}
+          component={SignUp}
+        />
+        <Stack.Screen
+          name="Profile"
+          options={{
+            title: 'الملف الشخصي',
+          }}
+          component={Profile}
+        />
+        <Stack.Screen 
+          name="Doctors"
+          options={{
+            title: "الأطباء",
+          }}
+          component={Doctors}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
